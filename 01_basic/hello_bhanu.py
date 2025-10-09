53e332940a4a846522485a6f0435da4a2c6414a4
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-10-09 10:11:53 +0530

    some data types done in theory

--- 01_basic/hello_bhanu.py
+++ 01_basic/hello_bhanu.py
@@ -4,3 +4,31 @@ print("Bhanu Pratap")
 def userName(name, age):
   print(f"my name is {name} and my age is {age}")
 
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+
+
+# ! repr , str, print 
+print(repr(chai_one))
+print(str(chai_two))
+print(chai_one)
+
+
+# * String
+numList = '0123456789'
+
+print(numList[:])
+print(numList[3:])
+print(numList[3:6])
+print(numList[0:7:2])
+print(numList[0:7:3])
+print(numList[0:7:4])
+
+
+chai = "masala_chai"
+print(chai.upper())
+
+
+newChai = "     ginger chai  "
+
+print(newChai.strip()) # ginger chai
